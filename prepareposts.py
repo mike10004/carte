42f1644938f6c1c729e20614ea801eda7dc4f9b9
Mike <mchaberski@gmail.com>
2014-06-01 15:04:28 -0400

    fixed parameters splitting bug

--- prepareposts.py
+++ prepareposts.py
@@ -6,10 +6,15 @@ Created on May 28, 2014
 @author: mchaberski
 '''
 
+import logging
+from argparse import ArgumentParser
 import csv
 import glob
 from jinja2 import Environment, PackageLoader
 import os.path
+import os
+
+_log = logging.getLogger('prepareposts')
 
 POST_TEMPLATES_DIR = '_posts_templates'
 POST_SOURCES_DIR = '_posts_sources'
@@ -18,9 +23,11 @@ env = Environment(loader=PackageLoader('prepareposts', POST_TEMPLATES_DIR))
 
 def format_parameters_element(parameters_str):
   if parameters_str:
+    _log.debug("formatting parameters string %s", repr(parameters_str))
     parameters = []
     for parameter_str in parameters_str.split("\n"):
-      attributes = parameter_str.split(' ', 3)
+      attributes = parameter_str.split(' ', 2)
+      _log.debug("parsed parameter string %s into %d attributes", repr(parameter_str), len(attributes))
       pname, ptype, pdescription = attributes[0], '', ''
       if len(attributes) == 2:
         pdescription = attributes[1]
@@ -65,16 +72,18 @@ def load_content(kind, content_dir=POST_SOURCES_DIR):
     print >> sys.stderr, 'skipping', filename
     return []
 
-def construct_filename(content, kind):
+AUTO_GENERATED_MARKER = 'x'
+
+def construct_filename(content, kind, marker=AUTO_GENERATED_MARKER):
     year, month, day = tuple([int(content[k]) for k in ('year', 'month', 'day')])
-    extension = os.path.splitext(glob.glob(os.path.join(POST_TEMPLATES_DIR, kind + '.*')))[1]
+    extension = os.path.splitext(_find_template_filename(kind))[1]
     if kind == 'endpoint':
       category = content['category'].lower()
       method = content['method'].lower()
-      return "%04d-%02d-%02d-x-%s-%s%s" % (year, month, day, method, category, extension)
+      return "%04d-%02d-%02d-%s-%s-%s%s" % (year, month, day, marker, method, category, extension)
     elif kind == 'type':
       type_name = content['type_name'].lower()
-      return "%04d-%02d-%02d-x-%s-%s%s" % (year, month, day, 'type', type_name, extension)
+      return "%04d-%02d-%02d-%s-%s-%s%s" % (year, month, day, marker, 'type', type_name, extension)
 
 def coalesce_type_fields(pagedefs):
   typedefs = {}
@@ -121,9 +130,28 @@ def load_kinds():
   template_files = glob.glob(os.path.join(POST_TEMPLATES_DIR, '*.*'))
   return [os.path.splitext(os.path.basename(f))[0] for f in template_files]
 
+def _find_template_filename(kind):
+  pattern = os.path.join(POST_TEMPLATES_DIR, kind + '.*')
+  return os.path.basename(glob.glob(pattern)[0])
+
+def clean_posts(marker=AUTO_GENERATED_MARKER):
+  generated_posts = glob.glob('_posts/????-??-??-' + marker + '-*.*')
+  for f in generated_posts:
+    os.remove(f)
+    _log.debug(" removed %s", f)
+  _log.info(" %d posts cleaned", len(generated_posts))
+
 def main():
+  parser = ArgumentParser()
+  parser.add_argument("--clean", action="store_true", help="delete generated posts before generating new ones")
+  parser.add_argument("--log-level", choices=('debug', 'info', 'warn', 'error'), help="set logging level", default='info')
+  parser.add_argument("--debug", action="store_const", dest="log_level", const='debug')
+  args = parser.parse_args()
+  logging.basicConfig(level=eval('logging.' + args.log_level.upper()))
+  if args.clean:
+    clean_posts()
   for kind in load_kinds():
-    template = env.get_template(kind + '.md')
+    template = env.get_template(_find_template_filename(kind))
     pagedefs = load_content(kind)
     render_contents(template, pagedefs, kind)
   return 0
